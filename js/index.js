b3bcbbc65ef1bc2df13cb8facb9ec73de3fd165b
Dove-White <dugbazahredeemer@gmail.com>
2023-10-02 13:23:43 +0000

    add images with javascript

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let myNav = document.getElementById("for-phone");
+let closeNav = document.getElementById("close-nav-icon");
 let myList = false;
 
 function openCloseNav() {
@@ -10,3 +11,41 @@ function openCloseNav() {
     myList = false;
   }
 }
+
+
+// Add image to html with javascript
+
+// all images and their styles
+let images = [
+  "img/blue-shoe.png",
+  "img/yellow-shoe.png",
+  "img/black-shoe.png",
+  "img/red-shoe.png",
+  "img/white-shoe.png",
+];
+const shoeStyle = "w-[200px] png-shadow group-hover:-scale-x-100 transition-all";
+
+// For blue White Shoe
+let blueShoe = document.getElementById('blue-shoe');
+blueShoe.src = images[0];
+blueShoe.className = shoeStyle;
+
+// For yellow gray shoe
+let yellowShoe = document.getElementById("yellow-shoe");
+yellowShoe.src = images[1];
+yellowShoe.className = shoeStyle;
+
+// For black white shoe 
+let blackShoe = document.getElementById("black-shoe");
+blackShoe.src = images[2];
+blackShoe.className = shoeStyle;
+
+// For red white shoe 
+let redShoe = document.getElementById("red-shoe");
+redShoe.src = images[3];
+redShoe.className = shoeStyle;
+
+// For white white shoe 
+let whiteShoe = document.getElementById("white-shoe");
+whiteShoe.src = images[4];
+whiteShoe.className = shoeStyle;
